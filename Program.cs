bfee996f69693ebaa2bec6604058fb1592880efc
shinshilkina <48287436+shinshilkina@users.noreply.github.com>
2019-09-08 00:42:26 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using Renci.SshNet;
+using System.Text;
 
 namespace SFTPConsole
 {
@@ -8,36 +9,126 @@ namespace SFTPConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Список содержимого sftp: \r\n");
-            ListDirectory("host", " username",  "password");
-            //string localFile = Console.ReadLine();
-            string localFile = Console.ReadLine();
-            localFile = Convert.ToString(Directory.GetCurrentDirectory() + localFile);
-            DownloadFile(localFile, "host", " username", "password");
-            if (File.Exists(Convert.ToString( Directory.GetCurrentDirectory()+localFile)))
+            //Console.WriteLine("L - list directory \r\nD - downlad file from sftp\r\nDEL - delete file from sftp\r\nU - upload file to sftp\r\nE- exit from app");
+            string choose = "";
+            do
             {
-                Console.WriteLine("success");
-            }
-            else { Console.WriteLine("fail!"); }
+                Console.WriteLine("L - list directory \r\nD - downlad file from sftp\r\nDEL - delete file from sftp\r\nU - upload file to sftp\r\nE- exit from app");
+                choose = Console.ReadLine();
+                //Console.WriteLine("filename:");
+                string localFile = "";
+                //localFile = Convert.ToString(Directory.GetCurrentDirectory() + localFile);
+                switch (choose)
+                {
+                    case "L":
+                        ListDirectory("host",  "username",  "password");
+                        break;
+                    case "D":
+                        localFile = "";
+                        DownloadFile(localFile, "host", "username", "password");
+                        break;
+                    case "U":
+                        Console.Write("filename: ");
+                        localFile = "\\" + Console.ReadLine();
+                        localFile = Convert.ToString(Directory.GetCurrentDirectory() + localFile);
+                        UploadFile(localFile, "host", "username", "password");
+                        break;
+                    case "DEL":
+                        Console.Write("filename: ");
+                        localFile = "\\" + Console.ReadLine();
+                        localFile = Convert.ToString(Directory.GetCurrentDirectory() + localFile);
+                        DeleteFile(localFile, "host", "username", "password");
+                        break;
+                }
+                Console.WriteLine("-----------------------------------------");
+            } while (choose!="E");
+         Console.Read();
         }
         /// <summary>
         /// This sample will download a file on the remote system to your local machine.
         /// </summary>
         public static void DownloadFile(string localFile, string host, string username, string password)
         {
-            string localFileName = System.IO.Path.GetFileName(localFile);
-            string remoteFileName = "";
-
-            using (var sftp = new SftpClient(host, username, password))
+            try
             {
-                sftp.Connect();
-
-                using (var file = File.OpenWrite(localFileName))
+                Console.WriteLine("Enter local filename: ");
+                localFile = "\\" + Console.ReadLine();
+                localFile = Convert.ToString(Directory.GetCurrentDirectory() + localFile);
+                // Delete the file if it exists.
+                if (File.Exists(localFile))
                 {
-                    sftp.DownloadFile(remoteFileName, file);
+                    File.Delete(localFile);
                 }
 
-                sftp.Disconnect();
+                //Create a file to write to.
+                
+                string localFileName = System.IO.Path.GetFileName(localFile);
+                Console.WriteLine("Enter sftp filename: ");
+                string remoteFileName = Console.ReadLine();
+
+                using (var sftp = new SftpClient(host, username, password))
+                {
+                    sftp.Connect();
+
+                    using (var file = File.OpenWrite(localFile))
+                    {
+                        sftp.DownloadFile(remoteFileName, file);
+                    }
+
+                    sftp.Disconnect();
+                    sftp.Dispose();
+                }
+            }
+            catch { }
+        }
+        public static void UploadFile(string localFile, string host, string username, string password)
+        {
+            FileInfo f = new FileInfo(localFile);
+            string uploadfile = f.FullName;
+            Console.WriteLine(f.Name);
+            Console.WriteLine("uploadfile: " + uploadfile+"\r\n");
+
+            //Passing the sftp host without the "sftp://"
+            var client = new SftpClient(host, username, password);
+            client.Connect();
+            if (client.IsConnected)
+            {
+
+                var fileStream = new FileStream(uploadfile, FileMode.Open);
+                if (fileStream != null)
+                {
+                    //If you have a folder located at sftp://ftp.example.com/share
+                    //then you can add this like:
+                    client.UploadFile(fileStream, "/root/" + f.Name, null);
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                fileStream.Close();
+            }
+        }
+        public static void DeleteFile(string localFile, string host, string username, string password)
+        {
+            FileInfo f = new FileInfo(localFile);
+            string uploadfile = f.FullName;
+            Console.WriteLine(f.Name);
+            Console.WriteLine("deletefile: " + uploadfile + "\r\n");
+
+            //Passing the sftp host without the "sftp://"
+            var client = new SftpClient(host, username, password);
+            client.Connect();
+            if (client.IsConnected)
+            {
+
+                var fileStream = new FileStream(uploadfile, FileMode.Open);
+                if (fileStream != null)
+                {
+                    //If you have a folder located at sftp://ftp.example.com/share
+                    //then you can add this like:
+                    client.DeleteFile("/root/" + f.Name);
+                    client.Disconnect();
+                    client.Dispose();
+                }
+                fileStream.Close();
             }
         }
 
